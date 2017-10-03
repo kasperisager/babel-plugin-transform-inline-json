c45a000dd23789f0b9008b58beac8cdf427615ae
Kasper Isager <kasperisager@gmail.com>
2017-10-03 15:42:35 +0200

    Add more tests

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -17,3 +17,39 @@ test('transforms a JSON import to inline JavaScript', t => {
 
   t.snapshot(code);
 });
+
+test('allows whitelisting files that should be inlined', t => {
+  const {code} = transform(
+    `
+    import foo from './fixtures/foo.json'
+    import bar from './fixtures/bar.json'
+    `,
+    {
+      filename: __filename,
+      plugins: [
+        [
+          plugin,
+          {
+            include: 'f*.json'
+          }
+        ]
+      ]
+    }
+  );
+
+  t.snapshot(code);
+});
+
+test('throws an exception when a JSON import cannot be found', t => {
+  t.throws(() => transform(
+    `
+    import bar from './fixtures/bar.json'
+    `,
+    {
+      filename: __filename,
+      plugins: [
+        plugin
+      ]
+    }
+  ));
+});
